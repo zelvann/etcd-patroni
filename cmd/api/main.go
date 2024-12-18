0af8792aeae86ca47cc47d18f96611bdbf118fd6
Zelvan Abdi Wijaya <zelvan1701@gmail.com>
2024-12-18 16:22:52 +0700

    fix: use multiple bucket

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,11 +1,8 @@
 package main
 
 import (
-	"context"
-	"fmt"
 	"log"
 
-	"github.com/minio/minio-go/v7"
 	"github.com/zelvann/minio-ds/internal/config"
 	"github.com/zelvann/minio-ds/internal/domain/icmp"
 	"github.com/zelvann/minio-ds/internal/domain/image"
@@ -22,16 +19,7 @@ func main() {
 		log.Fatalf("Failed to create minio client: %v", err)
 	}
 
-	// Check MinIO connection
-	buckets, err := minioClient.ListBuckets(context.Background())
-	if err != nil {
-		log.Fatalf("Failed to list buckets: %v", err)
-	}
-	fmt.Printf("Connected to MinIO. Available buckets: %v\n", buckets)
-
 	db := instance.NewPostgres(env.DBHost, env.DBUser, env.DBPassword, env.DBName, env.DBPort)
-
-	// Auto-migrate the product model
 	if err := db.AutoMigrate(&product.Product{}); err != nil {
 		log.Fatalf("Failed to migrate database: %v", err)
 	}
@@ -40,20 +28,6 @@ func main() {
 	server.Use(middleware.CORS())
 
 	icmp.Route(server)
-
-	// Create images bucket if it doesn't exist
-	err = minioClient.MakeBucket(context.Background(), "images", minio.MakeBucketOptions{})
-	if err != nil {
-		exists, errBucketExists := minioClient.BucketExists(context.Background(), "images")
-		if errBucketExists == nil && exists {
-			log.Printf("Bucket 'images' already exists")
-		} else {
-			log.Fatalf("Failed to create bucket: %v", err)
-		}
-	} else {
-		log.Printf("Successfully created 'images' bucket")
-	}
-
 	image.Route(server, minioClient)
 	product.Route(server, db, minioClient)
 
--- internal/instance/postgres.go
+++ internal/instance/postgres.go
@@ -9,10 +9,12 @@ import (
 )
 
 func NewPostgres(host, user, password, dbname, port string) *gorm.DB {
-	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable TimeZone=Asia/Jakarta",
-		host, user, password, dbname, port)
-	
-	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s TimeZone=Asia/Jakarta", host, user, password, dbname, port)
+
+	db, err := gorm.Open(postgres.New(postgres.Config{
+		DSN:                  dsn,
+		PreferSimpleProtocol: true,
+	}), &gorm.Config{})
 	if err != nil {
 		log.Fatalf("Failed to connect to database: %v", err)
 	}
